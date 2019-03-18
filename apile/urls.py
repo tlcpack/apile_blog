c34840dee4541ff19413b3b59a7e51da28373411
Taylor Cooke <taylorcooke@gmail.com>
2019-03-18 15:00:57 -0400

    initial commit.

--- apile/urls.py
+++ apile/urls.py
@@ -22,6 +22,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('core/', include('catalog.urls')),
+    path('core/', include('core.urls')),
     path('', RedirectView.as_view(url='/core/', permanent = True))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
