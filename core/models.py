412ce5509c35af60918f27ae4a4580c5784e06cc
Taylor Cooke <taylorcooke@gmail.com>
2019-03-18 17:06:47 -0400

    setting up models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class Post(models.Model):
+    """Model representing a user post"""
+
+    # title of post
+    title = models.CharField(max_length=250)
+
+    # author of post - User - ForeignKey relationship. does it go here?
+
+    # text body of post
+    body = models.TextField(max_length=1000)
+
+    # URL link
+    url = models.URLField(max_length=255)
+
+    # date and time of post
+    date = models.DateTimeField(auto_now_add=True)
+    
+    # adjusting the ordering so most recent is on top
+    class Meta:
+        ordering = ['-date']
