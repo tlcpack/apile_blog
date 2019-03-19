70d2f26cb0358a4a3328a541ffae7c2cebc328a8
Taylor Cooke <taylorcooke@gmail.com>
2019-03-19 11:54:29 -0400

    added urls. added admin site info

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.Index.as_view(), name='index'),
+    path('core/post/<int:pk>'), views.PostDetailView.as_view(), name='post-detail',
     
 ]
