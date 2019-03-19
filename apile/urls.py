8041bd4f5461458a99389a1580bb950a7dc98592
Taylor Cooke <taylorcooke@gmail.com>
2019-03-19 13:45:05 -0400

    registration working

--- apile/urls.py
+++ apile/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/',include('registration.backends.simple.urls')),
     path('core/', include('core.urls')),
     path('', RedirectView.as_view(url='/core/', permanent = True))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
