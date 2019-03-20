e72875cf2c96c82daa8274be0630f9e333616c07
Rob Taylor <rob.taylor543@gmail.com>
2019-03-20 14:37:42 -0400

    Modified migrations to include User instead of Author

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Post, Author, Comment
+from .models import Post, Comment
 
 # Register your models here.
 admin.site.register(Post)
-admin.site.register(Author)
 admin.site.register(Comment)
--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
     path('core/post/<int:pk>', views.PostDetailView.as_view(), name='post-detail'),
-    path('core/author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+    path('core/author/<int:pk>', views.AuthorDetailView.as_view(), name='user-detail'),
 ]
