29fa501ceca34905b597893119d4a4b1f2c5f1c8
Taylor Cooke <taylorcooke@gmail.com>
2019-03-22 13:27:07 -0400

    able to create post. created post can be edited and favorited

--- core/forms.py
+++ core/forms.py
@@ -6,3 +6,10 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('content',)
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ('title', 'content', 'url',)
+        # may need to allow users a blank URL in case they are posting non-linking posts
