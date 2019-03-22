2e91b7518f8ac4e745a6cb6cbf068abefa397c98
Taylor Cooke <taylorcooke@gmail.com>
2019-03-22 11:12:34 -0400

    commenting to post works

--- core/views.py
+++ core/views.py
@@ -58,9 +58,10 @@ def comment_new(request, pk):
         if form.is_valid():
             comment = form.save(commit=False)
             comment.author = request.user
+            comment.post = post
             comment.save()
             return redirect('post-detail', pk=post.pk)
     else:
         form = CommentForm()
-    return render(request, '/comment_new.html', {'form':form, 'post':post})
+    return render(request, 'comment_new.html', {'form':form, 'post':post})
 
